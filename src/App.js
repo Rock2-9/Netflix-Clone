80f262ef233b02486906c387449c3a0d1be39982
Deepak-29-y <85682413+Deepak-29-y@users.noreply.github.com>
2022-02-09 02:32:33 +0530

    Almost done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Row from "./components/Row";
+import requests from "./requests";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Making Netflix Clone</h1>
+      <Row
+        category_url={requests.fetchNetflixOriginal}
+        title="NETFLIX ORIGINALS"
+        isLarge={true}
+      />
+      <Row category_url={requests.fetchTrending} title="Trending Movies" />
+
+      <Row category_url={requests.fetchTopRated} title="Top Rated Movies" />
+      <Row category_url={requests.fetchActionMovies} title="Acton Movies" />
+      <Row category_url={requests.fetchComedyMovies} title="Comedy Movies" />
+      <Row category_url={requests.fetchHorrorMovies} title="Horror Movies" />
+      <Row category_url={requests.fetchRomanceMovies} title="Romantic Movies" />
+      <Row
+        category_url={requests.fetchDocumentaries}
+        title="Documentary Movies"
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,20 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import "./row.css";
 
-function Row() {
+function Row(props) {
+  const { category_url, title, isLarge } = props;
+  const [movie, setMovie] = useState({ img_url: [] });
+  const base_url = "https://api.themoviedb.org/3";
+  const fetchUrl = async () => {
+    const request = await fetch(`${base_url}${category_url}`);
+    const parsedData = await request.json();
+    const movie_title = parsedData.results.map((e) => {
+      return e.original_title;
+    });
+    const poster_path = parsedData.results.map((e) => {
+      return e.poster_path;
+    });
+    console.log(movie_title);
+
+    setMovie({ img_url: poster_path });
+
+    console.log(movie.img_url);
+  };
+
   useEffect(() => {
-    const fetchurl = async () => {
-      const request = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=ce79f3e685011079a163a28b583b1277&with_genre=35"
-      );
-      const parsedData = await request.json();
-      parsedData.results.map((e) => {
-        console.log(e.original_title);
-      });
-    };
-    fetchurl();
+    fetchUrl();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="movie">
+        <div className={isLarge ? "movie_large_content" : "movie_content"}>
+          {movie.img_url.map((x) => (
+            <img src={`https://image.tmdb.org/t/p/w500${x}`} alt="" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Row;
